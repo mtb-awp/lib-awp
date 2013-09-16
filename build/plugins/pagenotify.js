aa1b90c8c371857164da058a8c50108a98993861
wuzhong <wuzhong@taobao.com>
2013-09-16 14:53:10 +0800

    init

---
+++ build/plugins/pagenotify.js
@@ -0,0 +1,1 @@
+!function(a,b){b.plugin.pagenotify={notify:function(a,c){var d=b.module.Page.get(a);d&&(d.notifyMessage=c)},onPageHide:function(a){a.notifyMessage&&delete a.notifyMessage}}}(window,window.app);
---
+++ src/plugins/pagenotify.js
@@ -0,0 +1,27 @@
+/**
+ * @author wuzhong@taobao.com
+ * @since mixsln 4.3+
+ * @version 4.3
+ */
+(function(win, app){
+
+    app.plugin.pagenotify = {
+
+        /**
+         * 通知其他page
+         * @param fragment
+         * @param data  具体需要传递的数据，通过 page['notifyMessage'] 获取
+         */
+        notify: function(fragment, data) {
+            var page = app.module.Page.get(fragment);
+            if(page){
+                page['notifyMessage'] = data;
+            }
+        },
+
+        onPageHide : function(page, options) {
+            page['notifyMessage'] && (delete page['notifyMessage']);
+        }
+
+    }
+})(window, window['app']);
